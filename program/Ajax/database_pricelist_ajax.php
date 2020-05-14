0947eac3ef151f85ee5269b7b022a71385d742c3
hendralien32 <hendralien32@gmail.com>
2020-05-14 17:28:19 +0700

    Update 14/05/2020

--- program/Ajax/database_pricelist_ajax.php
+++ program/Ajax/database_pricelist_ajax.php
@@ -8,23 +8,23 @@
         <tbody>
             <tr>
                 <th width="5%">#</th>
-                <th width="15%">Nama Bahan</th>
-                <th width="15%">Jenis</th>
-                <th width="12%">Sisi</th>
-                <th width="6%">Warna</th>
-                <th width="6%">1 Lbr</th>
-                <th width="6%">2 Lbr</th>
-                <th width="6%">3-5 Lbr</th>
-                <th width="6%">6-9 Lbr</th>
-                <th width="6%">10 Lbr</th>
-                <th width="6%">20 Lbr</th>
-                <th width="6%">50 Lbr</th>
-                <th width="6%">100 Lbr</th>
-                <th width="6%">250 Lbr</th>
-                <th width="6%">500 Lbr</th>
-                <th width="6%">1 Ktk</th>
-                <th width="6%">2-19 Ktk</th>
-                <th width="6%">20 ktk</th>
+                <th width="10%">Nama Bahan</th>
+                <th width="10%">Jenis</th>
+                <th width="3%">Sisi</th>
+                <th width="3%">Warna</th>
+                <th width="4%">1 Lbr</th>
+                <th width="4%">2 Lbr</th>
+                <th width="4%">3-5 Lbr</th>
+                <th width="4%">6-9 Lbr</th>
+                <th width="4%">10 Lbr</th>
+                <th width="4%">20 Lbr</th>
+                <th width="5%">50 Lbr</th>
+                <th width="5%">100 Lbr</th>
+                <th width="5%">250 Lbr</th>
+                <th width="5%">500 Lbr</th>
+                <th width="5%">1 Ktk</th>
+                <th width="5%">2-19 Ktk</th>
+                <th width="4%">20 ktk</th>
                 <th width="6%">SPL Price</th>
                 <th width="1%"></th>
             </tr>

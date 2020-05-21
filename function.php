c25323b0c902278eab4fda1eded5f710c1af6b9d
hendralien32 <hendralien32@gmail.com>
2020-05-21 21:40:10 +0700

    Update 21/05/2020

--- function.php
+++ function.php
@@ -3,16 +3,14 @@ date_default_timezone_set('Asia/Jakarta');
 
 $conn   = mysqli_connect("localhost", "root", "", "new_ypm") or die(mysqli_error());
 $conn_OOP = new mysqli("localhost", "root", "", "new_ypm");
-// Check connection
 if ($conn_OOP->connect_error) {
     die("Connection failed: " . $conn_OOP->connect_error);
 }
 
-$conn_Server = new mysqli("192.168.1.1:3306", "root", "yes4531203", "new_ypm");
-// Check connection
-if ($conn_Server->connect_error) {
-    die("Connection failed: " . $conn_Server->connect_error);
-}
+// $conn_Server = new mysqli("192.168.1.1", "root", "yes4531203", "new_ypm");
+// if ($conn_Server->connect_error) {
+//     die("Connection failed: " . $conn_Server->connect_error);
+// }
 
 $date   = date('Y-m-d');
 $months = date('Y-m');

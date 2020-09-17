8e6f5a879b57bf94487a6e4e28c9b77d33238fe4
hendralien32 <hendralien32@gmail.com>
2020-09-17 08:03:14 +0700

    update data

--- program/Ajax/Laporan_StockBrg_ajax.php
+++ program/Ajax/Laporan_StockBrg_ajax.php
@@ -37,7 +37,12 @@ require_once "../../function.php";
                 barang.satuan,
                 Brg_Masuk.barang_masuk,
                 Brg_Keluar.barang_keluar,
-                ( digital.jam + digital.qty_cetak + digital.error + digital_KodeBrg.jam + digital_KodeBrg.qty_cetak + digital_KodeBrg.error ) as digital_keluar
+                digital.Qty as digital_qty,
+                digital_KodeBrg.Qty as digitalKodeBrg_qty,
+                OLD_digital.Qty as OLD_digital_qty,
+                OLD_digital_KodeBrg.Qty as OLD_digitalKodeBrg_qty,
+                OLD_Brg_Masuk.barang_masuk as OLD_barang_masuk,
+                OLD_Brg_Keluar.barang_keluar as OLD_barang_keluar
             FROM
                 barang
             LEFT JOIN 
@@ -47,14 +52,15 @@ require_once "../../function.php";
                 FROM
                     flow_barang
                 WHERE
-                    left(flow_barang.tanggal, 7)>='2020-09' AND 
-                    left(flow_barang.tanggal, 7)<='2020-09' AND
+                    left(flow_barang.tanggal, 7)>='$_POST[dari_bulan]' AND 
+                    left(flow_barang.tanggal, 7)<='$_POST[ke_bulan]' AND
                     flow_barang.hapus != 'Y'
                 GROUP BY 
                     flow_barang.ID_Bahan
                 ) as Brg_Masuk
             ON
                 Brg_Masuk.ID_Bahan = barang.id_barang
+
             LEFT JOIN 
                 (SELECT
                     flow_barang.ID_Bahan,
@@ -62,8 +68,8 @@ require_once "../../function.php";
                 FROM
                     flow_barang
                 WHERE
-                    left(flow_barang.tanggal, 7)>='2020-09' AND 
-                    left(flow_barang.tanggal, 7)<='2020-09' AND
+                    left(flow_barang.tanggal, 7)>='$_POST[dari_bulan]' AND 
+                    left(flow_barang.tanggal, 7)<='$_POST[ke_bulan]' AND
                     flow_barang.hapus != 'Y'
                 GROUP BY 
                     flow_barang.ID_Bahan
@@ -74,22 +80,22 @@ require_once "../../function.php";
             LEFT JOIN 
                 (SELECT
                     digital_printing.id_bahan,
-                    SUM(digital_printing.jam) as jam,
+                    (SUM(digital_printing.jam) +
                     SUM((CASE
                         WHEN digital_printing.hitungan_click = '1' THEN ROUND(digital_printing.qty_cetak * 1)
                         WHEN digital_printing.hitungan_click = '2' THEN ROUND(digital_printing.qty_cetak / 2)
                         ELSE ROUND(digital_printing.qty_cetak / 2)
-                    END)) as qty_cetak,
+                    END)) +
                     SUM((CASE
                         WHEN digital_printing.hitungan_click = '1' THEN ROUND(digital_printing.error * 1)
                         WHEN digital_printing.hitungan_click = '2' THEN ROUND(digital_printing.error / 2)
                         ELSE ROUND(digital_printing.error / 2)
-                    END)) as error
+                    END))) as Qty
                 FROM
                     digital_printing
                 WHERE
-                    left(digital_printing.tgl_cetak, 7)>='2020-09' AND 
-                    left(digital_printing.tgl_cetak, 7)<='2020-09'
+                    left(digital_printing.tgl_cetak, 7)>='$_POST[dari_bulan]' AND 
+                    left(digital_printing.tgl_cetak, 7)<='$_POST[ke_bulan]'
                 GROUP BY 
                     digital_printing.id_bahan
                 ) digital
@@ -99,27 +105,105 @@ require_once "../../function.php";
             LEFT JOIN 
                 (SELECT
                     digital_printing.kode_bahan,
-                    SUM(digital_printing.jam) as jam,
+                    (SUM(digital_printing.jam) +
                     SUM((CASE
                         WHEN digital_printing.hitungan_click = '1' THEN ROUND(digital_printing.qty_cetak * 1)
                         WHEN digital_printing.hitungan_click = '2' THEN ROUND(digital_printing.qty_cetak / 2)
                         ELSE ROUND(digital_printing.qty_cetak / 2)
-                    END)) as qty_cetak,
+                    END)) +
                     SUM((CASE
                         WHEN digital_printing.hitungan_click = '1' THEN ROUND(digital_printing.error * 1)
                         WHEN digital_printing.hitungan_click = '2' THEN ROUND(digital_printing.error / 2)
                         ELSE ROUND(digital_printing.error / 2)
-                    END)) as error
+                    END))) as Qty
                 FROM
                     digital_printing
                 WHERE
-                    left(digital_printing.tgl_cetak, 7)>='2020-09' AND 
-                    left(digital_printing.tgl_cetak, 7)<='2020-09'
+                    left(digital_printing.tgl_cetak, 7)>='$_POST[dari_bulan]' AND 
+                    left(digital_printing.tgl_cetak, 7)<='$_POST[ke_bulan]'
                 GROUP BY 
                     digital_printing.kode_bahan
                 ) digital_KodeBrg
             ON
                 digital_KodeBrg.kode_bahan = barang.kode_barang
+
+            LEFT JOIN 
+                (SELECT
+                    flow_barang.ID_Bahan,
+                    SUM(flow_barang.barang_masuk) as barang_masuk
+                FROM
+                    flow_barang
+                WHERE
+                    left(flow_barang.tanggal, 7)<'$_POST[dari_bulan]' AND
+                    flow_barang.hapus != 'Y'
+                GROUP BY 
+                    flow_barang.ID_Bahan
+                ) as OLD_Brg_Masuk
+            ON
+                OLD_Brg_Masuk.ID_Bahan = barang.id_barang
+
+            LEFT JOIN 
+                (SELECT
+                    flow_barang.ID_Bahan,
+                    SUM(flow_barang.barang_keluar) as barang_keluar
+                FROM
+                    flow_barang
+                WHERE
+                    left(flow_barang.tanggal, 7)<'$_POST[dari_bulan]' AND
+                    flow_barang.hapus != 'Y'
+                GROUP BY 
+                    flow_barang.ID_Bahan
+                ) as OLD_Brg_Keluar
+            ON
+                OLD_Brg_Keluar.ID_Bahan = barang.id_barang
+
+            LEFT JOIN 
+                (SELECT
+                    digital_printing.id_bahan,
+                    (SUM(digital_printing.jam) +
+                    SUM((CASE
+                        WHEN digital_printing.hitungan_click = '1' THEN ROUND(digital_printing.qty_cetak * 1)
+                        WHEN digital_printing.hitungan_click = '2' THEN ROUND(digital_printing.qty_cetak / 2)
+                        ELSE ROUND(digital_printing.qty_cetak / 2)
+                    END)) +
+                    SUM((CASE
+                        WHEN digital_printing.hitungan_click = '1' THEN ROUND(digital_printing.error * 1)
+                        WHEN digital_printing.hitungan_click = '2' THEN ROUND(digital_printing.error / 2)
+                        ELSE ROUND(digital_printing.error / 2)
+                    END))) as Qty
+                FROM
+                    digital_printing
+                WHERE
+                    left(digital_printing.tgl_cetak, 7)<'$_POST[dari_bulan]'
+                GROUP BY 
+                    digital_printing.id_bahan
+                ) OLD_digital
+            ON
+                OLD_digital.id_bahan = barang.id_barang
+
+            LEFT JOIN 
+                (SELECT
+                    digital_printing.kode_bahan,
+                    (SUM(digital_printing.jam) +
+                    SUM((CASE
+                        WHEN digital_printing.hitungan_click = '1' THEN ROUND(digital_printing.qty_cetak * 1)
+                        WHEN digital_printing.hitungan_click = '2' THEN ROUND(digital_printing.qty_cetak / 2)
+                        ELSE ROUND(digital_printing.qty_cetak / 2)
+                    END)) +
+                    SUM((CASE
+                        WHEN digital_printing.hitungan_click = '1' THEN ROUND(digital_printing.error * 1)
+                        WHEN digital_printing.hitungan_click = '2' THEN ROUND(digital_printing.error / 2)
+                        ELSE ROUND(digital_printing.error / 2)
+                    END))) as Qty
+                FROM
+                    digital_printing
+                WHERE
+                    left(digital_printing.tgl_cetak, 7)<'$_POST[dari_bulan]'
+                GROUP BY 
+                    digital_printing.kode_bahan
+                ) OLD_digital_KodeBrg
+            ON
+                OLD_digital_KodeBrg.kode_bahan = barang.kode_barang
 
             WHERE
                 barang.jenis_barang = 'KRTS'
@@ -132,17 +216,23 @@ require_once "../../function.php";
         if ($result->num_rows > 0) :
             while ($d = $result->fetch_assoc()) :
                 $no++;
-                $stock_keluar = $d['barang_keluar'] + $d['digital_keluar'];
+                $stock_lama = $d['OLD_barang_masuk'] - $d['OLD_digitalKodeBrg_qty'] - $d['OLD_digital_qty'] - $d['OLD_barang_keluar'];
+                $stock_keluar = $d['barang_keluar'] + $d['digital_qty'] + $d['digitalKodeBrg_qty'];
+                $sisa_stock = $d['barang_masuk'] + $stock_lama - $stock_keluar;
+
+                if($sisa_stock<$d['min_stock']) { $alert="background-color:#e74c3c; color:white;"; }
+			    else { $alert=""; }
                 echo "
-                    <tr>
+                    <tr style='$alert'>
                         <td>$no</td>
                         <td>$d[nama_barang]</td>
                         <td>$d[kode_barang]</td>
                         <td>$d[jenis_barang]</td>
-                        <td class='a-right' style='padding-right:15px'>" . number_format($d['min_stock']) . " $d[satuan]</td>
-                        <td class='a-right' style='padding-right:15px'></td>
-                        <td class='a-right' style='padding-right:15px'>" . number_format($d['barang_masuk']) . " $d[satuan]</td>
-                        <td class='a-right' style='padding-right:15px'>" . number_format($stock_keluar) . " $d[satuan]</td>
+                        <td class='a-right' style='padding-right:15px'><b>" . number_format($d['min_stock']) . "</b> $d[satuan]</td>
+                        <td class='a-right' style='padding-right:15px'><b>" . number_format($stock_lama) . "</b> $d[satuan]</td>
+                        <td class='a-right' style='padding-right:15px'><b>" . number_format($d['barang_masuk']) . "</b> $d[satuan]</td>
+                        <td class='a-right' style='padding-right:15px'><b>" . number_format($stock_keluar) . "</b> $d[satuan]</td>
+                        <td class='a-right' style='padding-right:15px'><b>" . number_format($sisa_stock) . "</b> $d[satuan]</td>
                     </tr>
                 ";
             endwhile;

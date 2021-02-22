af841657f38c639522951cf0aa9508e2be5baddf
hendralien32 <hendralien32@gmail.com>
2021-02-22 18:12:21 +0700

    still progress

--- program_new/sales_order.php
+++ program_new/sales_order.php
@@ -17,10 +17,11 @@ require_once '../function.php';
 <div class='content-table'>
     <table class='table-list'>
         <tr>
-            <th width="1%">#</th>
+            <th width="2%">#</th>
+            <th width="8%">Tanggal</th>
             <th width="6%">Order ID</th>
-            <th width="50%">Description</th>
-            <th width="10%">Icon</th>
+            <th width="43%">Description</th>
+            <th width="8%">Icon</th>
             <th width="2%">Sisi</th>
             <th width="15%">Bahan</th>
             <th width="8%">Qty</th>
@@ -28,17 +29,23 @@ require_once '../function.php';
             <th width="3%"> </th>
         </tr>
         <?php
-        $n = 1;
+        $n = 0;
 
         $sql =
             "SELECT
                 penjualan.oid,
-                penjualan.sisi,
+                penjualan.waktu as tanggal,
+                UPPER(LEFT(penjualan.kode, 1)) as code,
                 (CASE
-                    WHEN penjualan.sisi = '1' THEN 'satu'
-                    WHEN penjualan.sisi = '2' THEN 'dua'
-                    ELSE ''
-                END) as css_sisi,
+                    WHEN penjualan.kode = 'large format' THEN 'lf'
+                    WHEN penjualan.kode = 'digital' THEN 'dp'
+                    WHEN penjualan.kode = 'indoor' THEN 'il'
+                    WHEN penjualan.kode = 'Xuli' THEN 'ix'
+                    WHEN penjualan.kode = 'offset' THEN 'o'
+                    WHEN penjualan.kode = 'etc' THEN 'e'
+                    ELSE 'e'
+                END) as kode_barang,
+                penjualan.sisi,
                 (CASE
                     WHEN penjualan.status = 'selesai' THEN 'Y'
                     WHEN penjualan.status = '' THEN 'N'
@@ -50,10 +57,9 @@ require_once '../function.php';
                     WHEN barang.id_barang > 0 THEN barang.nama_barang
                     ELSE penjualan.bahan
                 END) as bahan,
-                CONCAT(penjualan.qty, ' ' ,penjualan.satuan) as qty,
+                CONCAT('<b>', penjualan.qty, '</b> ' ,penjualan.satuan) as qty,
                 (CASE
-                    WHEN penjualan.panjang > 0 THEN CONCAT('Uk. ', penjualan.panjang, ' X ', penjualan.lebar, ' Cm')
-                    WHEN penjualan.lebar > 0 THEN CONCAT('Uk. ', penjualan.panjang, ' X ', penjualan.lebar, ' Cm')
+                    WHEN penjualan.panjang > 0 || penjualan.lebar > 0 THEN CONCAT('Uk. ', penjualan.panjang, ' X ', penjualan.lebar, ' Cm')
                     ELSE ''
                 END) as ukuran,
                 penjualan.ditunggu,
@@ -64,15 +70,10 @@ require_once '../function.php';
                 END) as acc,
                 penjualan.Design,
                 penjualan.description,
-                LEFT( penjualan.waktu, 10 ) as tanggal,
-                LEFT(penjualan.kode, 1) as code,
-                penjualan.kode as kode_barang,
                 customer.nama_client,
+                customer.no_telp,
                 setter.nama as Nama_Setter,
                 penjualan.cancel,
-                penjualan.posisi_file,
-                penjualan.img_design,
-                penjualan.file_design,
                 (CASE
                     WHEN penjualan.pembayaran = 'lunas' THEN 'Y'
                     ELSE 'N'
@@ -85,7 +86,7 @@ require_once '../function.php';
             from
                 penjualan
             LEFT JOIN 
-                (select customer.cid, customer.nama_client from customer) customer
+                (select customer.cid, customer.nama_client, customer.no_telp from customer) customer
             ON
                 penjualan.client = customer.cid  
             LEFT JOIN 
@@ -113,41 +114,82 @@ require_once '../function.php';
             // output data of each row
             while ($d = $result->fetch_assoc()) :
                 $n++;
+                $tanggal = date_format(date_create($d['tanggal']), 'j M Y');
+                $Waktu = date_format(date_create($d['tanggal']), 'h:i A');
+                $No_telp = "$d[no_telp]";
+
+                if (strlen($d['no_telp']) == 12) :
+                    $telp = sprintf(
+                        "- %s-%s-%s",
+                        substr($No_telp, 0, 4),
+                        substr($No_telp, 4, 4),
+                        substr($No_telp, 8)
+                    );
+                elseif (strlen($d['no_telp']) == 10) :
+                    $telp = sprintf(
+                        "- %s-%s-%s",
+                        substr($No_telp, 0, 4),
+                        substr($No_telp, 4, 3),
+                        substr($No_telp, 7)
+                    );
+                elseif (strlen($d['no_telp']) == 7) :
+                    $telp = sprintf(
+                        "- (061) %s-%s",
+                        substr($No_telp, 0, 3),
+                        substr($No_telp, 3, 4)
+                    );
+                else :
+                    $telp = "";
+                endif;
                 echo "
                 <tr>
-                    <td>$n</td>
+                    <td><center>$n</center></td>
                     <td>
-                        <span>
-                            $d[oid]
-                        </span>
-                    </td>
-                    <td class='deskripsi'>
-                        <div class='lf'>
+                        <div class='test'>
+                            <span class='client_name'>
+                                <center>$tanggal</center>
+                            </span>
                             <span>
-                                X
+                                <center>$Waktu</center>
+                            </span>
+                        </div>
+                    </td>
+                    <td><center>$d[oid]</center></td>
+                    <td class='deskripsi'>
+                        <div class='$d[kode_barang]'>
+                            <span>
+                                $d[code]
                             </span>
                         </div>
                         <div class='test'>
                             <span class='client_name'>
-                                $d[nama_client]
+                                $d[nama_client] $telp
                             </span>
                             <span>
-                                $d[description]
+                                $d[description] $d[ukuran]
                             </span>
                         </div>
                     </td>
-                    <td>
-                        <div class='icon_detail'>
-                            <i class='fas fa-cash-register'></i>
-                            <i class='fas fa-cash-register'></i>
-                            <i class='fas fa-cash-register'></i>
-                            <i class='fas fa-cash-register'></i>
+                    <td class='icon>
+                        <div>
+                            <div class='icon_A>
+                                <i class='fas fa-cash-register'></i>
+                                <i class='fas fa-cash-register'></i>
+                                <i class='fas fa-cash-register'></i>
+                            </div>
+                        </div>
+                        <div>
+                            <div class='icon_B'>
+                                <i class='fas fa-cash-register'></i>
+                                <i class='fas fa-cash-register'></i>
+                                <i class='fas fa-cash-register'></i>
+                            </div>
                         </div>
                     </td>
-                    <td>1</td>
-                    <td>TIC 260gr</td>
-                    <td>1 Lembar</td>
-                    <td>Kristy</td>
+                    <td><center><b>$d[sisi]</b></center></td>
+                    <td>$d[bahan]</td>
+                    <td>$d[qty]</td>
+                    <td>$d[Nama_Setter]</td>
                     <td><i class='fas fa-cogs'></i></td>
                 </tr>
                 ";
